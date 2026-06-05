1e76a06faf623acab5a1a2886ed4b9072d4521ff
Esa Koskinen <esakoskin2@gmail.com>
2026-06-06 00:14:00 +0900

    chore: update fe-draft deployment to latest version

--- fe-draft/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rsync -av ./* ../studio-esagames-homepage/fe-draft/ --exclude fe-assets-db && (cd ../studio-esagames-homepage/; ga; gcm "chore: update fe-draft deployment to latest version"; gps)
